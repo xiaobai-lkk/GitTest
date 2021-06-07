78fab59275ede25692205df1418f4ea2ba8ec959
02 <2720807618@qq.com>
2021-06-07 23:58:03 +0800

    update02

--- src/main/java/com/atguigu/test/Hello.java
+++ src/main/java/com/atguigu/test/Hello.java
@@ -7,5 +7,7 @@ package com.atguigu.test;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("111111");
+
+        System.out.println("222222");
     }
 }
