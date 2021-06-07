6792cdb33a88d98936e0a70a5a90cce60f897a0f
01 <1960380001@qq.com>
2021-06-07 23:39:19 +0800

    update01

---
+++ src/main/java/com/atguigu/test/Hello.java
@@ -0,0 +1,11 @@
+package com.atguigu.test;
+
+/**
+ * @author lkk
+ * @create 2021-06-07 23:36
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("111111");
+    }
+}
